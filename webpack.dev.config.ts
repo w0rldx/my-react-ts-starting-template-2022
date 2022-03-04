bf48213467d93be6797cb5aa76afa56f632bacac
w0rldx <michaelgmx1@googlemail.com>
2022-03-04 19:33:27 +0100

    Include newest changes from my projekt

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -6,59 +6,83 @@ import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } fro
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
 interface WebPackDevConfiguration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
+    devServer?: WebpackDevServerConfiguration;
 }
 
 const config: WebPackDevConfiguration = {
-  mode: 'development',
-  output: {
-    publicPath: '/',
-  },
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-          },
-        },
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ['file-loader'],
-      },
+    mode: 'development',
+    output: {
+        publicPath: '/',
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-transform-runtime',
+                            '@babel/plugin-syntax-flow',
+                            '@babel/plugin-transform-react-jsx',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/Index.html',
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+        }),
+        new HotModuleReplacementPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-    }),
-    new HotModuleReplacementPlugin(),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.join(__dirname, 'build'),
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'build'),
+        },
+        historyApiFallback: true,
+        port: 3000,
+        open: {
+            app: {
+                name: 'google-chrome',
+            },
+        },
+        proxy: {
+            '/api': {
+                target: 'https://localhost:7298',
+                secure: false,
+                pathRewrite: { '^/api': '' },
+                logLevel: 'debug',
+            },
+        },
+    },
 };
 
 export default config;
